877dbe422afdec5118fe0418db81fcb88e19ce10
Aras Can Akin <aacanakin@gmail.com>
2016-01-26 00:57:00 +0200

    add generic type and common types

--- types.go
+++ types.go
@@ -1,24 +1,72 @@
 package qbit
 
-type T interface {
-	String(driver string) string
-}
+import "fmt"
 
 type Type struct {
-	size   int
-	String func(driver string) string
+	Sql func() string
 }
 
-func Integer() *T {
+// generates common smallint syntax
+func SmallInt() *Type {
 	return &Type{
-		String: func(driver string) {
-			if driver == "mysql" {
+		Sql: func() string {
+			return "SMALLINT"
+		},
+	}
+}
 
-			} else if driver == "postgresql" {
+// generates common int syntax
+func Int() *Type {
+	return &Type{
+		Sql: func() string {
+			return "INT"
+		},
+	}
+}
 
-			} else if driver == "sqlite" {
+// generates common bigint syntax
+func BigInt() *Type {
+	return &Type{
+		Sql: func() string {
+			return "BIGINT"
+		},
+	}
+}
 
-			}
+// generates common numeric type syntax
+// p: max number of all digits (both sides)
+// s: max number of digits after the decimal point
+func Numeric(p int, s int) *Type {
+	return &Type{
+		Sql: func() string {
+			return fmt.Sprintf("NUMERIC(%d, %d)", p, s)
+		},
+	}
+}
+
+// generates common char type syntax
+func Char(size int) *Type {
+	return &Type{
+		Sql: func() string {
+			return fmt.Sprintf("CHAR(%d)", size)
+		},
+	}
+}
+
+// generates common varchar type syntax
+func VarChar(size int) *Type {
+	return &Type{
+		Sql: func() string {
+			return fmt.Sprintf("VARCHAR(%d)", size)
+		},
+	}
+}
+
+// generates common text type syntax
+func Text() *Type {
+	return &Type{
+		Sql: func() string {
+			return "TEXT"
 		},
 	}
 }
