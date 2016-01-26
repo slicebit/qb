0e20bbd5741f76e5ece230e294b7484bbb3254fa
Aras Can Akin <aacanakin@gmail.com>
2016-01-26 21:06:44 +0200

    refactor NewColumn(), add Sql function of column

--- column.go
+++ column.go
@@ -1,16 +1,28 @@
 package qbit
+import (
+	"fmt"
+	"strings"
+)
 
 // function generates a table column
-func Column(name string, t Type, constraints []Constraint) *column {
-	return &column{
+func NewColumn(name string, t Type, constraints []Constraint) *Column {
+	return &Column{
 		Name:        name,
 		T:           t,
 		Constraints: constraints,
 	}
 }
 
-type column struct {
+type Column struct {
 	Name        string
 	T           Type
 	Constraints []Constraint
+}
+
+func (c *Column) Sql() string {
+	constraints := []string{}
+	for _, v := range c.Constraints {
+		constraints = append(constraints, v.Name)
+	}
+	return fmt.Sprintf("%s %s %s", c.Name, c.T.Sql(), strings.Join(constraints, " "))
 }
