3c50f9acab7325fe7c302d976236e8a799cceb7b
Aras Can Akin <aacanakin@gmail.com>
2016-01-26 23:14:40 +0200

    add initial table test

---
+++ table_test.go
@@ -0,0 +1,65 @@
+package qbit
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTable(t *testing.T) {
+
+	table := NewTable(
+		"user",
+		[]Column{
+			NewColumn(
+				"id",
+				BigInt(),
+				[]Constraint{
+					PrimaryKey(),
+				},
+			),
+			NewColumn(
+				"email",
+				VarChar(512),
+				[]Constraint{
+					Unique("email"),
+					NotNull(),
+				},
+			),
+			NewColumn(
+				"bio",
+				Text(),
+				[]Constraint{
+					NotNull(),
+				},
+			),
+			NewColumn(
+				"gender",
+				Char(16),
+				[]Constraint{
+					Default("female"),
+				},
+			),
+			NewColumn(
+				"birth_date",
+				Char(16),
+				[]Constraint{
+					NotNull(),
+				},
+			),
+		},
+		[]Constraint{},
+	)
+
+	q := fmt.Sprintf(`CREATE TABLE user(
+	id BIGINT PRIMARY KEY,
+	email VARCHAR(512) UNIQUE NOT NULL,
+	bio TEXT NOT NULL,
+	gender CHAR(16) DEFAULT %s,
+	birth_date CHAR(16) NOT NULL
+);`, "`female`")
+
+	assert.Equal(t, table.Sql(), q)
+
+	fmt.Println(table.Sql())
+}
