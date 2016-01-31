2fac65f0a5f83012d684af2608c6b66426625a7f
Aras Can Akin <aacanakin@gmail.com>
2016-01-31 13:15:17 +0200

    move mysql types into common, remove Float()

--- mysql/types.go
+++ mysql/types.go
@@ -6,27 +6,6 @@ import (
 	"strings"
 )
 
-// generates a float type in mysql syntax
-// if only p is specified, p is the binary precision.
-// if p and s are both specified, p is the maximum number of all digits (both sides of the decimal point),
-// s is the maximum number of digits after the point. p and s are optional
-func Float(p int, s int) *qbit.Type {
-	return &qbit.Type{
-		Sql: func() string {
-			return fmt.Sprintf("FLOAT(%d, %d)", p, s)
-		},
-	}
-}
-
-// generates a date type in mysql syntax
-func Date() *qbit.Type {
-	return &qbit.Type{
-		Sql: func() string {
-			return "DATE"
-		},
-	}
-}
-
 // generates a date time type in mysql syntax
 func DateTime() *qbit.Type {
 	return &qbit.Type{
@@ -49,33 +28,6 @@ func Time() *qbit.Type {
 	return &qbit.Type{
 		Sql: func() string {
 			return "TIME"
-		},
-	}
-}
-
-// generates a char type in mysql syntax
-func Char(size int) *qbit.Type {
-	return &qbit.Type{
-		Sql: func() string {
-			return fmt.Sprintf("CHAR(%d)", size)
-		},
-	}
-}
-
-// generates a varchar type in mysql syntax
-func VarChar(size int) *qbit.Type {
-	return &qbit.Type{
-		Sql: func() string {
-			return fmt.Sprintf("VARCHAR(%d)", size)
-		},
-	}
-}
-
-// generates a text type in mysql syntax
-func Text() *qbit.Type {
-	return &qbit.Type{
-		Sql: func() string {
-			return "TEXT"
 		},
 	}
 }
