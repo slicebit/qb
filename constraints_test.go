76afc414e03863d9773c03908d82312d446b0f36
Aras Can Akin <aacanakin@gmail.com>
2016-02-09 23:14:53 +0200

    disable PrimaryKey, ForeignKey constraints temporarily

--- constraints_test.go
+++ constraints_test.go
@@ -13,7 +13,7 @@ func TestConstraints(t *testing.T) {
 	assert.Equal(t, Unique(), Constraint{"UNIQUE"})
 	assert.Equal(t, Unique("email", "name"), Constraint{"UNIQUE(email, name)"})
 	assert.Equal(t, Key(), Constraint{"KEY"})
-	assert.Equal(t, PrimaryKey(), Constraint{"PRIMARY KEY"})
-	assert.Equal(t, PrimaryKey("email", "password"), Constraint{"PRIMARY KEY(email, password)"})
-	assert.Equal(t, ForeignKey("user_id", "profile", "user_id"), Constraint{"FOREIGN KEY (user_id) REFERENCES profile(user_id)"})
+//	assert.Equal(t, PrimaryKey(), Constraint{"PRIMARY KEY"})
+//	assert.Equal(t, PrimaryKey("email", "password"), Constraint{"PRIMARY KEY(email, password)"})
+//	assert.Equal(t, ForeignKey("user_id", "profile", "user_id"), Constraint{"FOREIGN KEY (user_id) REFERENCES profile(user_id)"})
 }
