a1a9384e8a1c16190449e367d13d9cda33254d4e
Aras Can Akin <aacanakin@gmail.com>
2016-02-09 23:15:21 +0200

    rename tagmapper into tag

---
+++ tag.go
@@ -0,0 +1,46 @@
+package qbit
+
+import (
+	"strings"
+	//	"fmt"
+	"errors"
+	"fmt"
+)
+
+type Tag struct {
+
+	// contains default, null, notnull, unique, primary_key, foreign_key(table.column), check(condition > 0)
+	Constraints []string
+
+	// contains type(size) or type parameters
+	Type string
+}
+
+func ParseTag(rawTag string) (*Tag, error) {
+
+	tag := &Tag{
+		Constraints: []string{},
+	}
+
+	if strings.Trim(rawTag, " ") == "" {
+		return tag, nil
+	}
+
+	tags := strings.Split(rawTag, ";")
+	for _, t := range tags {
+		tagKey := strings.Split(t, ":")
+		if len(tagKey) == 2 {
+			if tagKey[0] == "type" {
+				tag.Type = tagKey[1]
+			} else if tagKey[0] == "constraints" || tagKey[0] == "constraint" {
+				tag.Constraints = strings.Split(tagKey[1], ",")
+			} else {
+				return nil, errors.New("Invalid keyword in struct tag")
+			}
+		} else {
+			return nil, errors.New(fmt.Sprintf("Invalid tag key length, tag: %v", tag))
+		}
+	}
+
+	return tag, nil
+}
