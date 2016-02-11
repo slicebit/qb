08262d353727cce7e8a8508d2bc564792aba8f1a
Aras Can Akin <aacanakin@gmail.com>
2016-02-12 00:34:07 +0200

    add comments to session, fix lint errors

--- session.go
+++ session.go
@@ -1,15 +1,18 @@
 package qbit
 
+// NewSession generates a new Session given engine and returns session pointer
 func NewSession(engine *Engine) *Session {
 	return &Session{
 		engine: engine,
 	}
 }
 
+// Session is the composition of engine connection & orm mappings
 type Session struct {
 	engine *Engine
 }
 
+// Engine returns the current session engine
 func (s *Session) Engine() *Engine {
 	return s.engine
 }
