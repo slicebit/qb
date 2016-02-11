1f4ff8cd0cc186a0d565d7cb58df58733e907fd7
Aras Can Akin <aacanakin@gmail.com>
2016-02-11 23:28:38 +0200

    add new tests for new constraints

--- constraints_test.go
+++ constraints_test.go
@@ -10,4 +10,9 @@ func TestConstraints(t *testing.T) {
 	assert.Equal(t, NotNull(), Constraint{"NOT NULL"})
 	assert.Equal(t, Default(5), Constraint{"DEFAULT `5`"})
 	assert.Equal(t, Default("-"), Constraint{"DEFAULT `-`"})
+	assert.Equal(t, Unique(), Constraint{"UNIQUE"})
+	assert.Equal(t, Unique("email", "name"), Constraint{"UNIQUE(email, name)"})
+	assert.Equal(t, Primary(), Constraint{"PRIMARY KEY"})
+	assert.Equal(t, Primary("email", "password"), Constraint{"PRIMARY KEY(email, password)"})
+	assert.Equal(t, Foreign("user_id", "profile", "user_id"), Constraint{"FOREIGN KEY (user_id) REFERENCES profile(user_id)"})
 }
