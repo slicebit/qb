d3ae5f7ce0857d92d500572362282b93d3c6ee6e
Aras Can Akin <aacanakin@gmail.com>
2016-02-12 00:22:46 +0200

    add comments, fix engine lint errors

--- engine.go
+++ engine.go
@@ -2,6 +2,7 @@ package qbit
 
 import "database/sql"
 
+// NewEngine generates a new engine and returns it as an engine pointer
 func NewEngine(driver string, dsn string) (*Engine, error) {
 
 	conn, err := sql.Open(driver, dsn)
@@ -16,12 +17,14 @@ func NewEngine(driver string, dsn string) (*Engine, error) {
 	}, err
 }
 
+// Engine is the generic struct for handling db connections
 type Engine struct {
 	driver string
 	dsn    string
 	db     *sql.DB
 }
 
+// Exec executes insert & update type queries and returns sql.Result and error
 func (e *Engine) Exec(sql string, bindings []interface{}) (sql.Result, error) {
 
 	stmt, err := e.db.Prepare(sql)
@@ -37,18 +40,22 @@ func (e *Engine) Exec(sql string, bindings []interface{}) (sql.Result, error) {
 	return res, nil
 }
 
+// DB returns sql.DB of wrapped engine connection
 func (e *Engine) DB() *sql.DB {
 	return e.db
 }
 
+// Ping pings the db using connection and returns error if connectivity is not present
 func (e *Engine) Ping() error {
 	return e.db.Ping()
 }
 
+// Driver returns the driver as string
 func (e *Engine) Driver() string {
 	return e.driver
 }
 
+// Dsn returns the connection dsn
 func (e *Engine) Dsn() string {
 	return e.dsn
 }
