01b235fdc5cf90e242a9b74791bb3701805a16d8
Aras Can Akin <aacanakin@gmail.com>
2016-02-18 22:38:28 +0200

    add query delimiter tests

--- query_test.go
+++ query_test.go
@@ -16,4 +16,24 @@ func TestQuery(t *testing.T) {
 
 	assert.Equal(t, query.Clauses(), []string{"SELECT name", "FROM user", "WHERE id = ?"})
 	assert.Equal(t, query.Bindings(), []interface{}{5})
+	assert.Equal(t, query.SQL(), "SELECT name\nFROM user\nWHERE id = ?;")
+}
+
+func TestQueryWithDelimiter(t *testing.T) {
+
+	query := NewQuery()
+
+	assert.Equal(t, query.SQL(), "")
+
+	query.SetDelimiter(" ")
+
+	query.AddClause("SELECT name")
+	query.AddClause("FROM user")
+
+	query.AddClause("WHERE id = ?")
+	query.AddBinding(5)
+
+	assert.Equal(t, query.Clauses(), []string{"SELECT name", "FROM user", "WHERE id = ?"})
+	assert.Equal(t, query.Bindings(), []interface{}{5})
+	assert.Equal(t, query.SQL(), "SELECT name FROM user WHERE id = ?;")
 }
