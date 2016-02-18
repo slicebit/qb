388497b1b6385283097ab6f7f0aea71f2f01d110
Aras Can Akin <aacanakin@gmail.com>
2016-02-19 01:05:19 +0200

    add tests for metadata

---
+++ metadata_test.go
@@ -0,0 +1,49 @@
+package qb
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type UserMetadata struct {
+	ID int
+}
+
+func TestMetadata(t *testing.T) {
+
+	engine, _ := NewEngine("postgres", "user=root dbname=pqtest")
+	metadata := NewMetaData(engine)
+
+	metadata.Add(UserMetadata{})
+}
+
+type UserMetadataError struct {
+	ID int `qb:"constraints:i:nvalid"`
+}
+
+func TestMetadataAddError(t *testing.T) {
+
+	engine, _ := NewEngine("postgres", "user=root dbname=pqtest")
+	metadata := NewMetaData(engine)
+
+	assert.Panics(t, func() { metadata.Add(UserMetadataError{}) })
+	assert.Equal(t, len(metadata.Tables()), 0)
+}
+
+func TestMetadataAddTable(t *testing.T) {
+
+	engine, _ := NewEngine("postgres", "user=root dbname=pqtest")
+	metadata := NewMetaData(engine)
+
+	table := NewTable(
+		"user",
+		[]Column{
+			NewColumn("id", BigInt(), []Constraint{}),
+		},
+		[]Constraint{},
+	)
+
+	metadata.AddTable(table)
+
+	assert.Equal(t, metadata.Tables(), []*Table{table})
+}
