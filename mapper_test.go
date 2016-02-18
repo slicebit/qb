7daf9d3d2f735812b5073807aba59e873beaaf66
Aras Can Akin <aacanakin@gmail.com>
2016-02-19 00:36:55 +0200

    increase mapper tests coverage to 100%

--- mapper_test.go
+++ mapper_test.go
@@ -8,23 +8,24 @@ import (
 	"time"
 )
 
-type User struct {
-	ID          string     `qb:"constraints:primary_key"`
-	FacebookID  int64      `qb:"constraints:ref(facebook.id)"`
-	ProfileID   int64      `qb:"constraints:ref(profile.id)"`
-	ProfileName string     `qb:"constraints:ref(profile.name)"`
-	Email       string     `qb:"type:varchar(255); constraints:unique,notnull"`
-	FullName    string     `qb:"constraints:notnull"`
-	Password    string     `qb:"type:text"`
-	UserType    string     `qb:"constraints:default(guest)"`
-	CreatedAt   time.Time  `qb:"constraints:notnull"`
-	UpdatedAt   time.Time  `qb:"constraints:notnull"`
-	DeletedAt   *time.Time `qb:"constraints:null"`
-}
+type UnknownType struct{}
 
-type UserScore struct {
-	UserID string `qb:"constraints:ref(user.id),primary_key"`
-	Score  int64
+type User struct {
+	ID          string `qb:"constraints:primary_key"`
+	FacebookID  int64  `qb:"constraints:ref(facebook.id)"`
+	ProfileID   int64  `qb:"constraints:ref(profile.id)"`
+	ProfileName string `qb:"constraints:ref(profile.name)"`
+	Email       string `qb:"type:varchar(255); constraints:unique,notnull"`
+	FullName    string `qb:"constraints:notnull,default"`
+	Password    string `qb:"type:text"`
+	UserType    string `qb:"constraints:default(guest)"`
+	Premium     bool
+	CreatedAt   time.Time  `qb:"constraints:notnull"`
+	DeletedAt   *time.Time `qb:"constraints:null"`
+	Level       int
+	Money       float32
+	Score       float64
+	Unknown     UnknownType
 }
 
 func TestMapper(t *testing.T) {
@@ -32,11 +33,10 @@ func TestMapper(t *testing.T) {
 	mapper := NewMapper("mysql")
 
 	userTable, err := mapper.Convert(User{})
-	userScoreTable, err := mapper.Convert(UserScore{})
 
 	assert.Nil(t, err)
 	fmt.Println(userTable.SQL())
-	fmt.Println(userScoreTable.SQL())
+	//	fmt.Println(userScoreTable.SQL())
 }
 
 type UserErr struct {
@@ -53,4 +53,18 @@ func TestMapperError(t *testing.T) {
 	fmt.Println(err)
 	assert.NotNil(t, err)
 	assert.Empty(t, userErrTable)
+}
+
+type InvalidConstraint struct {
+	ID string `qb:"constraints:invalid_constraint"`
+}
+
+func TestMapperInvalidConstraint(t *testing.T) {
+
+	mapper := NewMapper("mysql")
+
+	invalidConstraintTable, err := mapper.Convert(InvalidConstraint{})
+
+	assert.Nil(t, invalidConstraintTable)
+	assert.NotNil(t, err)
 }
