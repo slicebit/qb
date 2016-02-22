4fbe5572fb5accf7f251eed044f265a07b5933e5
Aras Can Akin <aacanakin@gmail.com>
2016-02-23 00:36:26 +0200

    fix metadata errors, add Engine() function to return the current engine

--- metadata.go
+++ metadata.go
@@ -22,10 +22,16 @@ type MetaData struct {
 	mapper *Mapper
 }
 
+// Engine returns the currently bound engine of metadata
+func (m *MetaData) Engine() *Engine {
+	return m.engine
+}
+
 // Add retrieves the struct and converts it using mapper and appends to tables slice
 func (m *MetaData) Add(model interface{}) {
 
 	table, err := m.mapper.Convert(model)
+	fmt.Println(table.SQL())
 	if err != nil {
 		panic(err)
 	}
@@ -40,10 +46,6 @@ func (m *MetaData) AddTable(table *Table) {
 
 // Table returns the metadata registered table object. It returns nil if table is not found
 func (m *MetaData) Table(name string) *Table {
-
-	if m.engine.Driver() != "postgres" {
-		name = fmt.Sprintf("`%s`", name)
-	}
 
 	for _, t := range m.tables {
 		if t.name == name {
@@ -60,26 +62,25 @@ func (m *MetaData) Tables() []*Table {
 }
 
 // CreateAll creates all the tables added to metadata
-func (m *MetaData) CreateAll(engine *Engine) error {
+func (m *MetaData) CreateAll() error {
 
 	sqls := []string{}
 	for _, t := range m.tables {
 		sqls = append(sqls, t.SQL())
 	}
 
-	_, err := engine.DB().Exec(strings.Join(sqls, "\n"))
+	_, err := m.engine.DB().Exec(strings.Join(sqls, "\n"))
 	return err
 }
 
 // DropAll drops all the tables which is added to metadata
-func (m *MetaData) DropAll(engine *Engine) error {
+func (m *MetaData) DropAll() error {
 
 	sqls := []string{}
 	for i := len(m.tables) - 1; i >= 0; i-- {
 		sqls = append(sqls, fmt.Sprintf("DROP TABLE %s;", m.tables[i].Name()))
 	}
 
-	_, err := engine.DB().Exec(strings.Join(sqls, "\n"))
-
+	_, err := m.engine.DB().Exec(strings.Join(sqls, "\n"))
 	return err
 }
