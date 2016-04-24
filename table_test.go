7d56b4913c213b4c442bf2e79457e3eb2180e3d4
Aras Can Akin <aacanakin@gmail.com>
2016-04-25 01:05:27 +0300

    fix table tests not passing

--- table_test.go
+++ table_test.go
@@ -64,17 +64,17 @@ func TestTable(t *testing.T) {
 	table.AddRef("profile_id", "profile", "id")
 	table.AddRef("facebook_id", "user_facebook", "id")
 
-	q := "CREATE TABLE `user`(\n" +
-		"\t`id` BIGINT,\n" +
-		"\t`profile_id` BIGINT,\n" +
-		"\t`facebook_id` BIGINT,\n" +
-		"\t`email` VARCHAR(512) UNIQUE NOT NULL,\n" +
-		"\t`bio` TEXT NOT NULL,\n" +
-		"\t`gender` CHAR(16) DEFAULT 'female',\n" +
-		"\t`birth_date` CHAR(16) NOT NULL,\n" +
-		"\tPRIMARY KEY (`id`),\n" +
-		"\tFOREIGN KEY (`profile_id`) REFERENCES `profile`(`id`),\n" +
-		"\tFOREIGN KEY (`facebook_id`) REFERENCES `user_facebook`(`id`)\n);"
+	q := "CREATE TABLE user(\n" +
+		"\tid BIGINT,\n" +
+		"\tprofile_id BIGINT,\n" +
+		"\tfacebook_id BIGINT,\n" +
+		"\temail VARCHAR(512) UNIQUE NOT NULL,\n" +
+		"\tbio TEXT NOT NULL,\n" +
+		"\tgender CHAR(16) DEFAULT 'female',\n" +
+		"\tbirth_date CHAR(16) NOT NULL,\n" +
+		"\tPRIMARY KEY (id),\n" +
+		"\tFOREIGN KEY (profile_id) REFERENCES profile(id),\n" +
+		"\tFOREIGN KEY (facebook_id) REFERENCES user_facebook(id)\n);"
 
 	assert.Equal(t, table.SQL(), q)
 }
@@ -100,7 +100,7 @@ func TestTableInsert(t *testing.T) {
 
 	query := table.Insert(kv).Query()
 
-	assert.Contains(t, query.SQL(), "INSERT INTO `user`\n(")
+	assert.Contains(t, query.SQL(), "INSERT INTO user\n(")
 	assert.Contains(t, query.SQL(), "id")
 	assert.Contains(t, query.SQL(), "full_name")
 	assert.Contains(t, query.SQL(), ")\nVALUES (?, ?);")
@@ -128,7 +128,7 @@ func TestTableUpdate(t *testing.T) {
 		Where("id = ?", 1).
 		Query()
 
-	assert.Equal(t, query.SQL(), "UPDATE `user`\nSET `full_name` = ?\nWHERE id = ?;")
+	assert.Equal(t, query.SQL(), "UPDATE user\nSET full_name = ?\nWHERE id = ?;")
 	assert.Equal(t, query.Bindings(), []interface{}{"Aras", 1})
 }
 
@@ -151,6 +151,6 @@ func TestTableDelete(t *testing.T) {
 		Where("id = ?", 1).
 		Query()
 
-	assert.Equal(t, query.SQL(), "DELETE FROM `user`\nWHERE id = ?;")
+	assert.Equal(t, query.SQL(), "DELETE FROM user\nWHERE id = ?;")
 	assert.Equal(t, query.Bindings(), []interface{}{1})
 }
