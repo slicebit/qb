370bed75ecff4c6722477ec06d56e87d51c2479a
Christophe de Vienne <cdevienne@gmail.com>
2016-08-31 12:10:28 +0200

    Remove the old SQLClause

--- clause.go
+++ clause.go
@@ -4,22 +4,6 @@ type Clause interface {
 	Accept(context *CompilerContext) string
 }
 
-// Compiles is the standard interface for any compilable sql clause
-// Compiling means to post process any sql clauses if needed such as escaping, putting placeholders, etc.
-type Compiles interface {
-	// Build is the key function of any sql clause
-	// It returns sql as string and bindings as []interface{}
-	Build(dialect Dialect) (string, []interface{})
-}
-
-// SQLClause is the key interface for any sql clause
-type SQLClause interface {
-	Compiles
-	// String returns the dialect agnostic sql clause and bindings.
-	// It returns :varname as placeholders instead of $n or ?.
-	//String() (string, []interface{})
-}
-
 // TableSQLClause is the common interface for ddl generators such as Column(), PrimaryKey(), ForeignKey().Ref(), etc.
 type TableSQLClause interface {
 	// String takes the dialect and returns the ddl as an sql string
