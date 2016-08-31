2d9df93ca6b0ff2bced23ad6275893d993b9e6c0
Christophe de Vienne <cdevienne@gmail.com>
2016-08-31 20:40:14 +0200

    UpdateStmt is now a Clause
    SQL code is generated by the compiler, not the Build() function anymore.
    The Build() function remains for now at least.

--- update.go
+++ update.go
@@ -1,10 +1,5 @@
 package qb
 
-import (
-	"fmt"
-	"strings"
-)
-
 // Update generates an update statement and returns it
 // qb.Update(usersTable).
 // Values(map[string]interface{}{"id": 1}).
@@ -25,41 +20,19 @@ type UpdateStmt struct {
 	where     *WhereClause
 }
 
+// Accept implements Clause.Accept
+func (s UpdateStmt) Accept(context *CompilerContext) string {
+	return context.Compiler.VisitUpdate(context, s)
+}
+
 // Build generates a statement out of UpdateStmt object
 func (s UpdateStmt) Build(dialect Dialect) *Stmt {
 	defer dialect.Reset()
 
 	context := NewCompilerContext(dialect)
-
 	statement := Statement()
-	statement.AddSQLClause(fmt.Sprintf("UPDATE %s", dialect.Escape(s.table.Name)))
-	sets := []string{}
-	bindings := []interface{}{}
-	for k, v := range s.values {
-		sets = append(sets, fmt.Sprintf("%s = %s", dialect.Escape(k), dialect.Placeholder()))
-		bindings = append(bindings, v)
-	}
-
-	if len(sets) > 0 {
-		statement.AddSQLClause(fmt.Sprintf("SET %s", strings.Join(sets, ", ")))
-	}
-
-	if s.where != nil {
-		where := s.where.Accept(context)
-		statement.AddSQLClause(where)
-	}
-	bindings = append(bindings, context.Binds...)
-
-	returning := []string{}
-	for _, c := range s.returning {
-		returning = append(returning, dialect.Escape(c.Name))
-	}
-
-	if len(returning) > 0 {
-		statement.AddSQLClause(fmt.Sprintf("RETURNING %s", strings.Join(returning, ", ")))
-	}
-
-	statement.AddBinding(bindings...)
+	statement.AddSQLClause(s.Accept(context))
+	statement.AddBinding(context.Binds...)
 
 	return statement
 }
