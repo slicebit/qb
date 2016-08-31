1e7dea11e4a5ab0c3d5b5a62361c027dfd01d6a6
Christophe de Vienne <cdevienne@gmail.com>
2016-08-31 20:00:33 +0200

    DeleteStmt is now a Clause
    SQL code is generated by the compiler, not the Build() function anymore.
    The Build() function remains for now at least.

--- delete.go
+++ delete.go
@@ -1,10 +1,5 @@
 package qb
 
-import (
-	"fmt"
-	"strings"
-)
-
 // Delete generates a delete statement and returns it for chaining
 // qb.Delete(usersTable).Where(qb.Eq("id", 5))
 func Delete(table TableElem) DeleteStmt {
@@ -34,27 +29,19 @@ func (s DeleteStmt) Returning(cols ...ColumnElem) DeleteStmt {
 	return s
 }
 
+// Accept implements Clause.Accept
+func (s DeleteStmt) Accept(context *CompilerContext) string {
+	return context.Compiler.VisitDelete(context, s)
+}
+
 // Build generates a statement out of DeleteStmt object
 func (s DeleteStmt) Build(dialect Dialect) *Stmt {
 	defer dialect.Reset()
 
 	context := NewCompilerContext(dialect)
 	statement := Statement()
-	statement.AddSQLClause(fmt.Sprintf("DELETE FROM %s", dialect.Escape(s.table.Name)))
-	if s.where != nil {
-		where := s.where.Accept(context)
-		statement.AddSQLClause(where)
-	}
+	statement.AddSQLClause(s.Accept(context))
 	statement.AddBinding(context.Binds...)
-
-	returning := []string{}
-	for _, c := range s.returning {
-		returning = append(returning, dialect.Escape(c.Name))
-	}
-
-	if len(returning) > 0 {
-		statement.AddSQLClause(fmt.Sprintf("RETURNING %s", strings.Join(returning, ", ")))
-	}
 
 	return statement
 }
