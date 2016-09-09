e0ef5fe1e1addcbc443a3dedd4b3b775c3b51615
Christophe de Vienne <christophe@cdevienne.info>
2016-09-09 10:04:07 +0200

    Aggregate takes a Clause instead of a ColumnElem

--- aggregate.go
+++ aggregate.go
@@ -1,39 +1,39 @@
 package qb
 
-// Avg function generates "avg(%s)" statement for column
-func Avg(column ColumnElem) AggregateClause {
-	return Aggregate("AVG", column)
+// Avg function generates "avg(%s)" statement for clause
+func Avg(clause Clause) AggregateClause {
+	return Aggregate("AVG", clause)
 }
 
-// Count function generates "count(%s)" statement for column
-func Count(column ColumnElem) AggregateClause {
-	return Aggregate("COUNT", column)
+// Count function generates "count(%s)" statement for clause
+func Count(clause Clause) AggregateClause {
+	return Aggregate("COUNT", clause)
 }
 
-// Sum function generates "sum(%s)" statement for column
-func Sum(column ColumnElem) AggregateClause {
-	return Aggregate("SUM", column)
+// Sum function generates "sum(%s)" statement for clause
+func Sum(clause Clause) AggregateClause {
+	return Aggregate("SUM", clause)
 }
 
-// Min function generates "min(%s)" statement for column
-func Min(column ColumnElem) AggregateClause {
-	return Aggregate("MIN", column)
+// Min function generates "min(%s)" statement for clause
+func Min(clause Clause) AggregateClause {
+	return Aggregate("MIN", clause)
 }
 
-// Max function generates "max(%s)" statement for column
-func Max(column ColumnElem) AggregateClause {
-	return Aggregate("MAX", column)
+// Max function generates "max(%s)" statement for clause
+func Max(clause Clause) AggregateClause {
+	return Aggregate("MAX", clause)
 }
 
-// Aggregate generates a new aggregate clause given function & column
-func Aggregate(fn string, column ColumnElem) AggregateClause {
-	return AggregateClause{fn, column}
+// Aggregate generates a new aggregate clause given function & clause
+func Aggregate(fn string, clause Clause) AggregateClause {
+	return AggregateClause{fn, clause}
 }
 
 // AggregateClause is the base struct for building aggregate functions
 type AggregateClause struct {
 	fn     string
-	column ColumnElem
+	clause Clause
 }
 
 func (c AggregateClause) Accept(context *CompilerContext) string {
