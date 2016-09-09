9b9638e363375920b6bc56e323f95101557c338c
Christophe de Vienne <christophe@cdevienne.info>
2016-09-09 12:19:54 +0200

    Add WhereClause.And and WhereClause.Or
    There function makes it easier to add filters to an existing where
    clause

--- where_test.go
+++ where_test.go
@@ -14,3 +14,23 @@ func TestWhere(t *testing.T) {
 		"WHERE (X AND Y)", asDefSQL(
 			Where(SQLText("X"), SQLText("Y"))))
 }
+
+func TestWhereAnd(t *testing.T) {
+	assert.Equal(t,
+		"WHERE (X AND Y)", asDefSQL(
+			Where(SQLText("X")).And(SQLText("Y"))))
+	assert.Equal(t,
+		"WHERE (X AND Y AND Z)",
+		asDefSQL(
+			Where(SQLText("X")).And(SQLText("Y"), SQLText("Z"))))
+}
+
+func TestWhereOr(t *testing.T) {
+	assert.Equal(t,
+		"WHERE (X OR Y)", asDefSQL(
+			Where(SQLText("X")).Or(SQLText("Y"))))
+	assert.Equal(t,
+		"WHERE (X OR Y OR Z)",
+		asDefSQL(
+			Where(SQLText("X")).Or(SQLText("Y"), SQLText("Z"))))
+}
