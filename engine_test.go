71ca6a4435d8cd4c27c8673068aad4ee7c5fb95c
Christophe de Vienne <christophe@cdevienne.info>
2016-09-09 22:27:03 +0200

    Add transactions support

--- engine_test.go
+++ engine_test.go
@@ -64,3 +64,90 @@ func TestEngineFail(t *testing.T) {
 	_, err = engine.Exec(statement)
 	assert.NotNil(t, err)
 }
+
+func TestTx(t *testing.T) {
+	engine, err := New("sqlite3", ":memory:")
+	assert.Nil(t, err)
+	defer engine.Close()
+
+	engine.SetDialect(NewDialect("sqlite3"))
+
+	usersTable := Table(
+		"users",
+		Column("full_name", Varchar()).NotNull(),
+	)
+
+	_, err = engine.DB().Exec(usersTable.Create(engine.Dialect()))
+	assert.Nil(t, err)
+
+	countStmt := Select(Count(usersTable.C("full_name"))).From(usersTable)
+
+	tx, err := engine.Begin()
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, tx.tx, tx.Tx())
+
+	_, err = tx.Exec(
+		usersTable.Insert().
+			Values(map[string]interface{}{
+				"full_name": "Robert De Niro",
+			}),
+	)
+	assert.Equal(t, nil, err)
+	var count int
+	row := tx.QueryRow(countStmt)
+	assert.Nil(t, row.Scan(&count))
+	assert.Equal(t, 1, count)
+
+	tx.Commit()
+
+	row = engine.QueryRow(countStmt)
+	assert.Equal(t, nil, row.Scan(&count))
+	assert.Equal(t, 1, count)
+
+	tx, err = engine.Begin()
+	assert.Equal(t, nil, err)
+
+	_, err = tx.Exec(usersTable.Insert().
+		Values(map[string]interface{}{
+			"full_name": "Al Pacino",
+		}),
+	)
+	assert.Equal(t, nil, err)
+
+	rows, err := tx.Query(countStmt)
+	assert.Equal(t, nil, err)
+	assert.True(t, rows.Next())
+	assert.Equal(t, nil, rows.Scan(&count))
+	assert.Equal(t, 2, count)
+
+	tx.Rollback()
+
+	row = engine.QueryRow(countStmt)
+	assert.Equal(t, nil, row.Scan(&count))
+	assert.Equal(t, 1, count)
+
+	tx, err = engine.Begin()
+
+	assert.Nil(t, nil)
+
+	var user struct{ FullName string }
+	var users []struct{ FullName string }
+
+	assert.Nil(t,
+		tx.Get(usersTable.Select(usersTable.C("full_name")), &user),
+	)
+	assert.Equal(t, "Robert De Niro", user.FullName)
+
+	assert.Nil(t,
+		tx.Select(usersTable.Select(usersTable.C("full_name")), &users),
+	)
+	assert.Equal(t, "Robert De Niro", users[0].FullName)
+
+}
+
+func TestTxBeginError(t *testing.T) {
+	engine, err := New("postgres", "invalid_connexion")
+	_, err = engine.Begin()
+	assert.NotNil(t, err)
+}
